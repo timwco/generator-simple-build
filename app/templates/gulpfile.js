2948af3aa8fa07e684da7028b8eb73635bc8d4a1
Tim Whitacre <tim@timw.co>
2015-03-09 17:37:52 -0400

    Update to how Bower CSS deps are managed - simpler imo

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,6 +1,12 @@
 ;(function (){
-
+  
   'use strict';
+
+  // Put Your CSS Dependencies Here
+  var cssVendorFiles = 
+  [
+    'normalize-css/normalize.css'
+  ];
 
   // Require Specific Modules
   var gulp        = require('gulp'),
@@ -16,21 +22,20 @@
       transform   = require('vinyl-transform'),
       path        = require('path'),
       server      = tinylr(),
-      gulpif      = require('gulp-if'),
-      mbower      = require('main-bower-files');
+      gulpif      = require('gulp-if');
 
   // Convert SASS (.scss) files to CSS (.css)
   gulp.task('css', function() {
     return gulp.src('app/styles/*.scss')
-      .pipe(
-        sass( {
+      .pipe( 
+        sass( { 
           includePaths: ['app/styles'],
           errLogToConsole: true
         } ) )
       .pipe(gulp.dest('dist/styles/') )
       .pipe(livereload( server ));
   });
-
+   
   // Browseriy's, Uglify's, & Concats JS
   // Note, we only Uglify when production is true
   function js(production) {
@@ -45,7 +50,7 @@
       .pipe(gulp.dest('dist/scripts/'))
       .pipe(livereload(server));
   }
-
+   
   // Process .jade files
   gulp.task('jade', function() {
     return gulp.src('app/**/*.jade')
@@ -54,22 +59,29 @@
       .pipe(livereload(server));
   });
 
-  // Bower Dependencies
-  gulp.task('bower-files', function() {
-    return gulp.src(mbower())
-        .pipe(gulp.dest('dist/styles/vendor'));
+  // Bower Dependencies 
+  gulp.task('bower-css', function() {
+    cssVendorFiles = cssVendorFiles.map(function (f) {
+      return 'bower_components/' + f;
+    });
+    return gulp.src(cssVendorFiles)
+      .pipe(concat('vendor.css'))
+      .pipe(gulp.dest('dist/styles/'))
+      .pipe(livereload(server));
   });
 
-  // Starts an express server
+
+  // Starts an express server 
   gulp.task('server', function() {
     var spawn = require('child_process').spawn
+
     app.use(require('connect-livereload')());
     app.use(express.static(path.resolve('./dist')));
     app.listen(8000);
     spawn('open', ['http://localhost:8000']);
     return console.log('Listening on port: 8000');
   });
-
+   
   // Sets live-reload to watch the files
   gulp.task('listen', function () {
     server.listen(35729, function (err) {
@@ -78,20 +90,22 @@
     gulp.watch('app/styles/*.scss',['css']);
     gulp.watch('app/scripts/*.js',['js']);
     gulp.watch('app/**/*.jade',['jade']);
+        
   });
 
   // Watch Task
   // Watches files and runs live-reload
-  gulp.task('watch', ['css', 'jade', 'server', 'bower-files', 'listen'], function () {
+  gulp.task('watch', ['css', 'jade', 'server', 'bower-css', 'listen'], function () {
     return js(false);
   });
 
   // Build Task
-  gulp.task('build', ['css', 'jade', 'bower-files'], function () {
+  gulp.task('build', ['css', 'jade', 'bower-css'], function () {
     return js(true);
   });
 
   // Default Task
   gulp.task('default', ['build']);
+  
 
 }());
